4078157ba34e001d7c6adc63490955b4e459641d
KevinYang <chgc.tw@gmail.com>
2017-05-05 00:04:29 +0800

    將共用的顯示部分抽離成共用元件

---
+++ src/app/share/photos/photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'augt-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.scss']
+})
+export class PhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -3,16 +3,20 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+import {ContactComponent} from './contact/contact.component';
+import {PhotosComponent} from './photos/photos.component';
+import {SquadComponent} from './squad/squad.component';
+
 @NgModule({
+  declarations: [ContactComponent, SquadComponent, PhotosComponent],
   imports: [
     CommonModule,
     FormsModule,
     HttpModule,
   ],
   exports: [
-    CommonModule,
-    FormsModule,
-    HttpModule,
+    CommonModule, FormsModule, HttpModule, ContactComponent, SquadComponent,
+    PhotosComponent
   ]
 })
 export class ShareModule {
