8bc7a45ddf53c870fec95af3fd1a56901eee81e5
chgc <chgc_kevin@hotmail.com>
2017-05-12 00:51:59 +0800

    將一些文字部分轉換成變數模式，以方便維護 增加 og meta

--- src/app/event/event2017/event2017.component.ts
+++ src/app/event/event2017/event2017.component.ts
@@ -7,9 +7,76 @@ import {Meta, Title} from '@angular/platform-browser';
   styleUrls: ['./event2017.component.scss']
 })
 export class Event2017Component implements OnInit {
-  constructor(private title: Title, private meta: Meta) {
-    this.title.setTitle('Angular Taiwan 2017 | Angular User Group Taiwan');
+  title: string = 'Angular Taiwan 2017';
+  description: string =
+      `Angular從Beta到正式，一直到現在的四版，也差不多有一年多的時間。這一年來，每個人在學習Angular的旅程上，不論是自己的小專案，或是公司的案子上，多少會遇到一些讓人無法忘記的痛苦經驗，或是解開成就的喜悅感。這次的活動，我們邀請大家來分享這一年來學習Angular的心得，內容也會很精彩，不能錯過!`;
+
+  speakers: ISpeaker[] = [
+    {
+      name: '洪名辰',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: 'Sam',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: 'Haru',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: 'Jimmy ho',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: 'Jeff',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: '潘拉拉',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    },
+    {
+      name: 'Will 保哥',
+      description:
+          `Lorem ipsum dolor sit amet, mutat graeco volumus ad eam, singulis patrioque comprehensam nam no. Mei cu dicat voluptaria
+        volumus.`,
+      photo: 'assets/img/img1.jpg'
+    }
+  ];
+  constructor(private Title: Title, private meta: Meta) {
+    this.Title.setTitle(`${this.title} | Angular User Group Taiwan`);
+    this.meta.addTags([
+      {property: 'og:url', content: 'https://angular.tw'}, {property: 'og:type', content: 'event'},
+      {property: 'og:title', content: this.title},
+      {property: 'og:description', content: this.description},
+      {property: 'og:image', content: 'Good Morning Harwood'}
+    ]);
   }
 
   ngOnInit() {}
+}
+
+interface ISpeaker {
+  name: string;
+  description: string;
+  photo: string
 }
